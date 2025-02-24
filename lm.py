76ad34199601a2fccb562c559961554cfc2c35b0
shriyansnaik <shriyans.naik@gmail.com>
2025-02-24 22:37:58 +0530

    changed env variable naming

--- lm.py
+++ lm.py
@@ -2,46 +2,46 @@ import dspy
 import os
 
 naikshriyans = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_naikshriyans'],
+                       api_key=os.environ['GEMINI_API_KEY_NAIKSHRIYANS'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 shriyansnaik = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_shriyansnaik'],
+                       api_key=os.environ['GEMINI_API_KEY_SHRIYANSNAIK'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 adityapatil = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_adityapatil'],
+                       api_key=os.environ['GEMINI_API_KEY_ADITYAPATIL'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 akshatsharma = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_akshatsharma'],
+                       api_key=os.environ['GEMINI_API_KEY_AKSHATSHARMA'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 glacias = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_glacias'],
+                       api_key=os.environ['GEMINI_API_KEY_GLACIAS'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 jbeans = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_jbeans'],
+                       api_key=os.environ['GEMINI_API_KEY_JBEANS'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 somaiya = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_somaiya'],
+                       api_key=os.environ['GEMINI_API_KEY_SOMAIYA'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 splitit = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_splitit'],
+                       api_key=os.environ['GEMINI_API_KEY_SPLITIT'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
 
 shriyansresearch = dspy.LM("openai/gemini-2.0-flash",
-                       api_key=os.environ['GEMINI_API_KEY_shriyansresearch'],
+                       api_key=os.environ['GEMINI_API_KEY_SHRIYANSRESEARCH'],
                        base_url=os.environ['GEMINI_BASE_API']
                        )
